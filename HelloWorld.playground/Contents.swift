6df46e9e8fed96f2a5f3eea821a2e76663802452
Montserrat Canedo Gutiérrez <vmontse_cgutierrez@hotmail.com>
2023-03-23 16:59:48 -0400

    first steps

---
+++ HelloWorld.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+var greeting = "Hello, Juan Gabriel"
+var person = " desde playground"
+
+print(greeting + person)
---
+++ var_let.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+//constant
+let maximunNumberOfLoginAttemps = 3
+
+//variable
+var currentLoginAttempt = 0
+var x = 0.0, y = 0.0, z = 0.0
+
+//declare without initialize
+var welcomeMessage :  String
+welcomeMessage = "Hello, how u doin?"
+
+var red, green, blue : Double
+
+//greek letters can be used as a variable
+let π = 3.1416
+
+print("The current number of login attemps is: \(currentLoginAttempt) of \(maximunNumberOfLoginAttemps) available")
